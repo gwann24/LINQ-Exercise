3b8305a2d7bf18f8dbfae724add9f439f6a9fde5
Greg Wanner <greg@tegogroup.com>
2024-03-07 14:47:50 -0500

    Added new list then displayed both unordered and ordered using LINQ

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace LINQ
+﻿using System.Linq;
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> countrySingers = new List<string>() { "Tim McGraw", "Dolly Parton", "George Strait", "Tom Jones",
+                "Alan Jackson", "Garth Brooks", "Loretta Lynn", "Tammy Wynette", "Carrie Underwood", "Faith Hill", "Charlie Rich",
+                "Brad Paisley"};
+            Console.WriteLine("List of Unorderd Country Singers");
+            countrySingers.ForEach(singer => { Console.WriteLine(singer); });
+            Console.WriteLine("\nList of Ordered Country Singers, Length of Name, Alphabetically by LastName");
+            countrySingers.OrderBy(singer => singer.Length).ThenBy(singer => singer.Substring(singer.LastIndexOf(' '),(singer.Length - singer.LastIndexOf(' ')))).ToList().ForEach(singer => Console.WriteLine(singer));
+           //Console.WriteLine("Hello, World!");
         }
     }
 }
